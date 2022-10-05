c5de18dc455c37d7638260dc5ca366b43020920f
babbeløg <90256662+babbelog@users.noreply.github.com>
2022-10-05 13:50:23 +0200

    added taxi_trip_av

--- data/timeprocessing.py
+++ data/timeprocessing.py
@@ -10,11 +10,23 @@ taxidata = pd.read_csv('data/datastubs/testtaxidata.csv')
 zonedata = pd.read_csv('data/zones/relevant_zones/relevant_zones.csv') #len of zonedata is 113
 zonedata = zonedata[['LocationID']]
 
-m = np.zeros((113,113))
-
+# Bike trip matrix
+bike_trip_avg = np.zeros((113,113))
 
 for i in range (0,len(zonedata)):
     for j in range (0,len(zonedata)):
         df = bikedata.loc[(bikedata['start zone loc id'] == zonedata.at[i, 'LocationID']) & (bikedata['end zone loc id'] == zonedata.at[j, 'LocationID'])]
         if len(df) !=0:
-            m[i,j] = df['tripduration'].mean()
+            bike_trip_avg[i,j] = df['tripduration'].mean()
+
+
+# Taxi trip matrix
+taxi_trip_avg = np.zeros((113,113))
+
+for i in range (0,len(zonedata)):
+    for j in range (0,len(zonedata)):
+        df = taxidata.loc[(taxidata['PULocationID'] == zonedata.at[i, 'LocationID']) & (taxidata['DOLocationID'] == zonedata.at[j, 'LocationID'])]
+        if len(df) !=0:
+            taxi_trip_avg[i,j] = df['tripduration'].mean()
+
+    print(taxi_trip_avg[i])
