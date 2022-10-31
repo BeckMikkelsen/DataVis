570446e919f0ce3295135e6b2c611628d227859c
BeckMikkelsen <nikolajbeckmikkelsen@outlook.com>
2022-10-31 12:14:38 +0100

    count taxi

--- data/timeprocessing.py
+++ data/timeprocessing.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from shapely.geometry import Point, Polygon, MultiPolygon
 import matplotlib.pyplot as plt
-import json
 
 # Load data
 bikedata = pd.read_csv('data/datastubs/testbikedata.csv')
