4c80c085af5a854e57e637dc9a62135a6d841ee0
BeckMikkelsen <nikolajbeckmikkelsen@outlook.com>
2022-10-31 15:41:18 +0100

    july data, add zone to bike

--- data/data processing.py
+++ data/data processing.py
@@ -3,10 +3,10 @@ import numpy as np
 from shapely.geometry import Point, Polygon, MultiPolygon
 import matplotlib.pyplot as plt
 from datetime import datetime
-nrows = 50000
+
 
 #bikedata
-bikedata = pd.read_csv('data/citibike/202101-citibike-tripdata.csv',nrows=nrows)
+bikedata = pd.read_csv('data/citibike/202107-citibike-tripdata.csv',nrows=5000,usecols=['started_at','ended_at','start_station_name','end_station_name','start_lat','start_lng','end_lat','end_lng'])
 
 #zone data
 zonedataraw = pd.read_csv('data/zones/taxi_zones.csv',)
@@ -18,21 +18,21 @@ zonearea_lat_lng = zonedata["the_geom"]
 zone_areas = []
 
 #taxi data
-taxidataraw = pd.read_csv('data/taxi/2021_Yellow_Taxi_Trip_Data.csv',nrows=nrows)
-taxidata = taxidataraw.loc[(taxidataraw['PULocationID'].isin(bikezones)) & (taxidataraw['DOLocationID'].isin(bikezones))].reset_index()
+# taxidataraw = pd.read_csv('data/taxi/2021_Yellow_Taxi_Trip_Data.csv')
+# taxidata = taxidataraw.loc[(taxidataraw['PULocationID'].isin(bikezones)) & (taxidataraw['DOLocationID'].isin(bikezones))].reset_index()
 
-def taxitrip_duration(taxidata):
-    dur_arr =[]
-    for i in range(0,len(taxidata)):
-        start = datetime.strptime(taxidata.at[i,'tpep_pickup_datetime'], '%m/%d/%Y %I:%M:%S %p')
-        end = datetime.strptime(taxidata.at[i,'tpep_dropoff_datetime'], '%m/%d/%Y %I:%M:%S %p')
-        dur_arr.append((end-start).total_seconds())
-    return dur_arr
+# def taxitrip_duration(taxidata):
+#     dur_arr =[]
+#     for i in range(0,len(taxidata)):
+#         start = datetime.strptime(taxidata.at[i,'tpep_pickup_datetime'], '%m/%d/%Y %I:%M:%S %p')
+#         end = datetime.strptime(taxidata.at[i,'tpep_dropoff_datetime'], '%m/%d/%Y %I:%M:%S %p')
+#         dur_arr.append((end-start).total_seconds())
+#     return dur_arr
 
-taxidata.insert(0, "tripduration", taxitrip_duration(taxidata=taxidata), True)
+# taxidata.insert(0, "tripduration", taxitrip_duration(taxidata=taxidata), True)
 
-#create a small sample of taxi data
-taxidata.to_csv('data/datastubs/testtaxidata.csv', index=False)
+# #create a small sample of taxi data
+# taxidata.to_csv('data/datastubs/testtaxidata.csv', index=False)
 
 
 
@@ -65,8 +65,8 @@ def get_zone_from_lat_lng(bikedata):
     bikeendzonearr =[]
     taxidurationarr = []
     for i in range(0, len(bikedata)):#len(bikestart_lat_lng)):
-        pstart = Point(bikedata.at[i,'start station longitude'], bikedata.at[i,'start station latitude'])
-        pend = Point(bikedata.at[i,'end station longitude'], bikedata.at[i,'end station latitude'])
+        pstart = Point(bikedata.at[i,'start_lng'], bikedata.at[i,'start_lat'])
+        pend = Point(bikedata.at[i,'end_lng'], bikedata.at[i,'end_lat'])
         for (j,geom) in enumerate(zone_areas):
             locID=zonedata.at[j,'LocationID']
             if type(geom) == MultiPolygon:
@@ -83,17 +83,22 @@ def get_zone_from_lat_lng(bikedata):
 
             if len(bikeendzonearr) == i+1 and len(bikestartzonearr) == i+1:
                 break
+        if i == len(bikeendzonearr):
+            bikeendzonearr.append(-1)
+        if i == len(bikestartzonearr):
+            bikestartzonearr.append(-1)
     return (bikestartzonearr,bikeendzonearr)
 
 
 (bikestartzonearr,bikeendzonearr) = get_zone_from_lat_lng(bikedata=bikedata)
-bikedata.insert(0, "start zone loc id",bikestartzonearr, True)
-bikedata.insert(1, "end zone loc id",bikeendzonearr, True)
+bikedata.insert(0, "start_zoneID",bikestartzonearr, True)
+bikedata.insert(1, "end_zoneID",bikeendzonearr, True)
 
 
 
 #create a small sample of bike data for testing
 bikedata.to_csv('data/datastubs/testbikedata.csv',index=False)
+
 
 
 def plotzones():
