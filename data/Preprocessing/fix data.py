53a30889b7100bd8edb2fdf7cf65b292fe26b517
BeckMikkelsen <nikolajbeckmikkelsen@outlook.com>
2022-11-01 10:58:26 +0100

    created july taxi data

--- data/Preprocessing/fix data.py
+++ data/Preprocessing/fix data.py
@@ -35,11 +35,11 @@ def create_summer_bike_data():
 
 def create_summer_taxi_data():
     for chunk in pd.read_csv('data/taxi/2021_Yellow_Taxi_Trip_Data.csv',chunksize=500000, usecols=['tpep_pickup_datetime','tpep_dropoff_datetime','passenger_count','trip_distance','PULocationID','DOLocationID']):
-        bongo =(chunk[chunk['tpep_pickup_datetime'].str.startswith('08')])
+        bongo =(chunk[chunk['tpep_pickup_datetime'].str.startswith('07')])
         #mongo =(chunk[chunk['tpep_dropoff_datetime'].str.startswith('06')])
         if len(bongo):
-            bongo.to_csv('data/taxi/summertaxi.csv',mode='a', index=False,header=False)
+            bongo.to_csv('data/taxi07.csv',mode='a', index=False,header=False)
 
 
-#create_summer_taxi_data()
+create_summer_taxi_data()
 
