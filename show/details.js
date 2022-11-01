b6e79c51b98fb8876bc66a2d740d5090ac41ab70
babbeløg <90256662+babbelog@users.noreply.github.com>
2022-11-01 12:26:01 +0100

    Vegalite

--- show/details.js
+++ show/details.js
@@ -1,55 +1,14 @@
+let spec = {
+  "width": 600,
+  "height": 200,
+  "padding": 5,
 
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  "data": {"url": "http://0.0.0.0:8000/data/bikedata_07_ID.csv"},
+  "mark": "bar",
+  "encoding": {
+    "x": {"timeUnit": "day", "field": "started_at", "type": "ordinal"},
+    "y": {"aggregate": "count", "field": "end_zoneID"}
+  }
+}
 
-// set the ranges
-var x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
-var y = d3.scaleLinear()
-          .range([height, 0]);
-          
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("#dataviz").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// get the data
-d3.csv('http://0.0.0.0:8000/show/test.csv').then(function(data) {
-
-  // format the data
-  data.forEach(function(d) {
-    d.tripduration = +d.tripduration;
-  });
-
-  // Scale the range of the data in the domains
-  x.domain(data.map(function(d) { return d.tpep_pickup_datetime; }));
-  y.domain([0, d3.max(data, function(d) { return d.tripduration; })]);
-
-  // append the rectangles for the bar chart
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.tpep_pickup_datetime); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.tripduration); })
-      .attr("height", function(d) { return height - y(d.tripduration); });
-
-  // add the x Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  // add the y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
-
-});
+vegaEmbed("#dataviz", spec, {mode: "vega"}).then(console.log).catch(console.warn);
